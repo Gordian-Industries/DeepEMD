1c29c3becdb7fe79c5d2ba70b2d59932e2935d83
Awais Nizamani <awaisnizamani125@gmail.com>
2024-04-08 22:49:47 +0800

    running corner up frames

--- eval.py
+++ eval.py
@@ -9,7 +9,7 @@ from Models.models.Network import DeepEMD
 from Models.utils import *
 from Models.dataloader.data_utils import *
 
-DATA_DIR='your/default/dataset/dir'
+DATA_DIR='datasets/'
 # DATA_DIR='/home/zhangchi/dataset'
 MODEL_DIR='deepemd_trained_model/miniimagenet/fcn/max_acc.pth'
 
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser()
 # about task
 parser.add_argument('-way', type=int, default=5)
 parser.add_argument('-shot', type=int, default=1)
-parser.add_argument('-query', type=int, default=15)  # number of query image per class
+parser.add_argument('-query', type=int, default=2)  # number of query image per class
 parser.add_argument('-dataset', type=str, default='miniimagenet', choices=['miniimagenet', 'cub','tieredimagenet','fc100','tieredimagenet_yao','cifar_fs'])
 parser.add_argument('-set', type=str, default='test', choices=['train','val', 'test'])
 # about model
@@ -70,7 +70,7 @@ model.eval()
 # test dataset
 test_set = Dataset(args.set, args)
 sampler = CategoriesSampler(test_set.label, args.test_episode, args.way, args.shot + args.query)
-loader = DataLoader(test_set, batch_sampler=sampler, num_workers=8, pin_memory=True)
+loader = DataLoader(test_set, batch_sampler=sampler, num_workers=0, pin_memory=True)
 tqdm_gen = tqdm.tqdm(loader)
 
 # label of query images
@@ -79,25 +79,92 @@ test_acc_record = np.zeros((args.test_episode,))
 label = torch.arange(args.way).repeat(args.query)
 label = label.type(torch.cuda.LongTensor)
 
-with torch.no_grad():
-    for i, batch in enumerate(tqdm_gen, 1):
-        data, _ = [_.cuda() for _ in batch]
-        k = args.way * args.shot
-        model.module.mode = 'encoder'
-        data = model(data)
-        data_shot, data_query = data[:k], data[k:]  # shot: 5,3,84,84  query:75,3,84,84
-        model.module.mode = 'meta'
-        if args.shot > 1:
-            data_shot = model.module.get_sfc(data_shot)
-        logits = model((data_shot.unsqueeze(0).repeat(num_gpu, 1, 1, 1, 1), data_query))
-        acc = count_acc(logits, label) * 100
-        ave_acc.add(acc)
-        test_acc_record[i - 1] = acc
-        m, pm = compute_confidence_interval(test_acc_record[:i])
-        tqdm_gen.set_description('batch {}: This episode:{:.2f}  average: {:.4f}+{:.4f}'.format(i, acc, m, pm))
+query_path, ref_path = "data/Corner/query_images", "data/Corner/reference_images"
+output_path = "data/Corner/outputs"
 
-    m, pm = compute_confidence_interval(test_acc_record)
-    result_list = ['test Acc {:.4f}'.format(ave_acc.item())]
-    result_list.append('Test Acc {:.4f} + {:.4f}'.format(m, pm))
-    print(result_list[0])
-    print(result_list[1])
+query_images, r_imgs = get_data(query_path, ref_path)
+images_path = os.listdir(ref_path)
+q_images = os.listdir(query_path)
+r_data = None
+batch_size = 5
+with torch.no_grad():
+    for indexes in range(len(q_images)):
+        q_img = query_images[indexes,:,:,:]
+        q_img = q_img.unsqueeze(0)
+        # print(query_images.shape)
+        model.module.mode = 'encoder'
+        q_data = model(q_img)
+        if r_data is None:
+            reference_images = []
+            logits_list = []
+            for start_range in  range(0, r_imgs.shape[0], batch_size):
+                if r_imgs.shape[0] > start_range+batch_size:
+                    model.module.mode = 'encoder'
+                    data = model(r_imgs[start_range:start_range+batch_size, :, :, :])
+                    reference_images.append(data)
+                    model.module.mode = 'meta'
+                    data = model((q_data.unsqueeze(0).repeat(num_gpu, 1, 1, 1, 1), data))
+                    data = torch.squeeze(data, dim=1)
+                    logits_list.append(data)
+                else:
+                    model.module.mode = 'encoder'
+                    data = model(r_imgs[start_range:, :, :, :])
+                    reference_images.append(data)
+                    model.module.mode = 'meta'
+                    data = model((q_data.unsqueeze(0).repeat(num_gpu, 1, 1, 1, 1), data))
+                    data = torch.squeeze(data, dim=1)
+                    logits_list.append(data)
+                    # reference_images.append(model(r_imgs[start_range:, :, :, :]))
+            # r_data = torch.cat(reference_images, dim=0)
+            logits = torch.cat(logits_list, dim=0)
+
+
+        # model.module.mode = 'meta'
+        # logits = model((q_data.unsqueeze(0).repeat(num_gpu, 1, 1, 1, 1), r_data))
+        # logits = torch.squeeze(logits, dim=1)
+        # print(logits.shape)
+        # print(logits.shape)
+        probabilities = torch.softmax(logits, dim=0)
+        # print(probabilities.shape)
+        top5_probabilities, top5_indices = torch.topk(probabilities, k=5)
+        top5_indices = top5_indices.tolist()
+        print(top5_indices,top5_probabilities)
+        # print(logits)
+        # pred = torch.argmax(logits, dim=0)
+        idx_out = len(os.listdir(output_path))
+
+        for id, idx in enumerate(top5_indices):
+            # print(images_path, idx, idx_out, id)
+            if not os.path.exists(f"{output_path}/output_000{idx_out}"):
+                os.mkdir(f"{output_path}/output_000{idx_out}")
+            shutil.copy2(f"{ref_path}/{images_path[idx]}",f"{output_path}/output_000{idx_out}/top_{id+1}.jpg")
+            shutil.copy2(f"{query_path}/{q_images[indexes]}", f"{output_path}/output_000{idx_out}/query_image.jpg")
+        # print("Prediction is ",idx, images_path[idx])
+
+
+
+
+
+
+# with torch.no_grad():
+#     for i, batch in enumerate(tqdm_gen, 1):
+#         data, _ = [_.cuda() for _ in batch]
+#         k = args.way * args.shot
+#         model.module.mode = 'encoder'
+#         data = model(data)
+#         data_shot, data_query = data[:k], data[k:]  # shot: 5,3,84,84  query:75,3,84,84
+#         model.module.mode = 'meta'
+#         if args.shot > 1:
+#             data_shot = model.module.get_sfc(data_shot)
+#         logits = model((data_shot.unsqueeze(0).repeat(num_gpu, 1, 1, 1, 1), data_query))
+#         acc = count_acc(logits, label) * 100
+#         ave_acc.add(acc)
+#         test_acc_record[i - 1] = acc
+#         m, pm = compute_confidence_interval(test_acc_record[:i])
+#         tqdm_gen.set_description('batch {}: This episode:{:.2f}  average: {:.4f}+{:.4f}'.format(i, acc, m, pm))
+#
+#     m, pm = compute_confidence_interval(test_acc_record)
+#     result_list = ['test Acc {:.4f}'.format(ave_acc.item())]
+#     result_list.append('Test Acc {:.4f} + {:.4f}'.format(m, pm))
+#     print(result_list[0])
+#     print(result_list[1])
